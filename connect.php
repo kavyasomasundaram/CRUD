4312c1a3bbdb1395b2c10cf8ea3061d3255168b4
Kavya <kavyasomasundaram5@gmail.com>
2025-10-01 12:52:56 +0530

    Updated CRUD project with extra product and fixed prices

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@ $con = new mysqli('localhost', 'root', '', 'crudoperation', 3307);
 
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
-} else {
-    echo "Connected successfully!";
 }
+
+// echo "Connected successfully!"; // Commented out or removed
 ?>
